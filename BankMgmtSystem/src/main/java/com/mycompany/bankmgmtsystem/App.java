4e7c0c51fa5f2825c4b67773a0e79eb0c5acb36e
Hailekeh22 <hailekjt@gmail.com>
2024-02-11 21:13:08 +0300

    started building the login login

--- BankMgmtSystem/src/main/java/com/mycompany/bankmgmtsystem/App.java
+++ BankMgmtSystem/src/main/java/com/mycompany/bankmgmtsystem/App.java
@@ -16,7 +16,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setTitle("Bank Management System");
-        scene = new Scene(loadFXML("primary"));
+        scene = new Scene(loadFXML("main"));
         stage.setScene(scene);
         stage.show();
     }
